8d0a7a7372f0d7a1f38fa9efab4c3f8337ead223
evergeenhasib <hasib2407@gmail.com>
2019-11-12 00:34:56 +0600

    Static files and polish

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Job
-# Create your views here.
+from django.http import FileResponse, Http404
+
 def home(request):
     jobs = Job.objects
     return render(request, 'jobs/home.html', {'jobs':jobs})
+
+# def resume(request):
+#     return render(request, 'jobs/resume.html')
+
+
+
+def resume(request):
+    try:
+        return FileResponse(open('portfolio/static/resume.pdf', 'rb'), content_type='application/pdf')
+    except FileNotFoundError:
+        raise Http404()
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Dhaka'
 
 USE_I18N = True
 
@@ -122,8 +122,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio/static/')
+]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
